23ba3cf4962eb54731d4684dd267aeb9e7ba3a71
Ewen <ewen.le-quere@epitech.eu>
2021-04-15 10:00:49 +0200

    [FIX] all include and add macro

--- include/my_src.h
+++ include/my_src.h
@@ -16,13 +16,25 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
-#include <errno.h> 
+#include <errno.h>
 #include <fcntl.h>
 #include <dirent.h>
 #include <ncurses.h>
+#include <stdbool.h>
+
+#include "my_printf.h"
+#include "my_str.h"
+#include "my_utils.h"
+
 
 #undef  EXIT_FAILURE
 #define EXIT_FAILURE 84
+
+#ifdef EXIT_ERROR
+#undef EXIT_ERROR
+#endif
+#define EXIT_ERROR 1
+
 
 typedef struct player_s
 {
