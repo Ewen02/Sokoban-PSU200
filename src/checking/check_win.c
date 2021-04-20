7edba91103d9cb2f3e7847bbf6fdb2a383c48974
Ewen <ewen.le-quere@epitech.eu>
2021-04-20 13:21:50 +0200

    [FIX] norme checking_win.c

--- src/checking/check_win.c
+++ src/checking/check_win.c
@@ -7,14 +7,22 @@
 
 #include "../../include/my_src.h"
 
+static int count(player_t *player, uint i, uint j)
+{
+    int count = 0;
+
+    if (player->tab[i][j] == 'O')
+        count++;
+    return count;
+}
+
 int check_win(player_t *player)
 {
     int counter = 0;
 
-    for (int i = 0; player->tab[i] != NULL; i++)
-        for (int j = 0; player->tab[i][j] != '\0'; j++)
-            if (player->tab[i][j] == 'O')
-                counter++;
+    for (uint i = 0; player->tab[i] != NULL; i++)
+        for (uint j = 0; player->tab[i][j] != '\0'; j++)
+            counter = count(player, i, j);
     if (counter == 0)
         return EXIT_SUCCESS;
     return EXIT_ERROR;
